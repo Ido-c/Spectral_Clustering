101722212f61c1a586a61e1c926dd52efebc4a9b
jacob <jacobgidron@mail.tau.ac.il>
2021-02-23 17:49:39 +0200

    QR_iteration_algorithm implementation

--- utils.py
+++ utils.py
@@ -9,7 +9,21 @@ from sklearn.datasets import make_blobs
 # The Eigengap Heuristic
 
 # Random data generator (from K and N)
-def save_data(vectors,clusters, d):
+def save_data(vectors, clusters, d):
     data = np.column_stack((vectors[0], clusters[1]))
     fmt = ("%f", "%f", "%f", "%d") if d == 3 else ("%f", "%f", "%d")
-    np.savetxt("draftinf.txt", data, delimiter=", ", fmt=fmt)
+    np.savetxt("data.txt", data, delimiter=", ", fmt=fmt)
+
+
+def QR_iteration_algorithm(A):
+    n = A.shape[0]
+    Q_bar = np.identity(n)
+    for i in range(n):
+        Q, R = (0, 0)  # run the gram shmidit algo
+        A = R * Q
+        new_Q_bar = Q_bar*Q
+        ep = abs(Q_bar - new_Q_bar)
+        if ep< 0.0001:
+            return (A,Q_bar)
+        Q_bar = new_Q_bar
+    return (A,Q_bar)
