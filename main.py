7e6d2f2267add2c8c4935dc66e1f73e148ce8687
Ido-c <its.ido@gmail.com>
2021-02-23 13:05:24 +0200

    argument assertions

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
 import numpy as np
-
+from sklearn.datasets import make_blobs
+'''
+maxcap
+'''
 MAX_CAP_N = 200
 MAX_CAP_K = 10
 
 
 def main(n, k, random):
-    if random:
+    if random: # choose at random from range [max_capacity/2, max_capacity]
         k = np.random.randint(MAX_CAP_K//2, MAX_CAP_K + 1)
         n = np.random.randint(MAX_CAP_N//2, MAX_CAP_N + 1)
 
-
-
-
-    # Parse commandline arguments
-
-    # assert arguments
+    #   arguments assertion
+    flag = False
+    if k > n:
+        print("# of clusters must be smaller than # of points")
+        flag = True
+    if k <= 0:
+        print("# of clusters must be a positive non-zero number")
+        flag = True
+    if n <= 0:
+        print("# of points must be a positive non-zero number")
+        flag = True
+    if flag:
+        return
 
     # Print max capacity
-
     # Generate random data with indexed data points
+
+
 
     # Create 1st txt file
 
