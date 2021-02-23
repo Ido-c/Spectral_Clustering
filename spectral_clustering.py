f8078675d872c39c52754d762cb95431c370ec2f
Ido-c <its.ido@gmail.com>
2021-02-23 14:15:50 +0200

    spectral clustering - computed matrices

--- spectral_clustering.py
+++ spectral_clustering.py
@@ -1,6 +1,22 @@
-# Form the weighted adjacency matrix W
+import numpy as np
+import math
+# Form the Weighted Adjacency Matrix W
 
-# Compute the normalized graph Laplacian and Lnorm
+def spectral_clustering(vectors, n, dim):
+    # Create the weighted adjacency matrix
+    WAM = np.zeros((n, n), dtype=np.float)
+    for i in range(1, n):
+        for j in range(i + 1, n):
+            WAM[i, j] = find_weight(vectors[i], vectors[j], dim)
+    WAM = WAM + WAM.T
+
+    # Compute the Diagonal Degree Matrix ^0.5
+    DDM = np.zeros((n, n), dtype=np.float)
+    for i in range(n):
+        DDM[i, i] = np.sum(WAM[i])**(-0.5)
+
+    # Compute the normalized graph Laplacian
+    Lnorm = np.identity(n) - DDM @ WAM @ DDM
 
 # Determine k and obtain the first k eigenvectors of Lnorm
 
@@ -11,3 +27,9 @@
 # Treating each row of T as a point in Rk, cluster them into k clusters via the K-means algorithm
 
 # Assign the original point xi to cluster j if and only if row i of the matrix T was assigned to cluster j
+
+
+def find_weight(x, y, dim):
+    dist = np.linalg.norm(x - y)
+    return math.exp(-(dist/2))
+
