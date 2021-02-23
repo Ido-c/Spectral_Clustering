62f47dafa4ed5fa1895c93dde471cf2ef0079a30
Ido-c <its.ido@gmail.com>
2021-02-23 17:01:23 +0200

    gram shmidt 1

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.datasets import make_blobs
+import utils
 '''
 maxcap
 '''
--- utils.py
+++ utils.py
@@ -3,6 +3,17 @@ from sklearn.datasets import make_blobs
 
 
 # Modified Gram-Shmidt
+def MGS(A, n):
+    R = np.zeros((n, n))
+    Q = np.zeros((n, n))
+    for i in range(n):
+        temp = np.linalg.norm(A[:, i])
+        R[i, i] = temp
+        col = A[:, i]/temp
+        Q[:, i] = col
+        for j in range(i+1, n):
+            R[i, j] = col*A[:, j]
+
 
 # QR iteration
 
