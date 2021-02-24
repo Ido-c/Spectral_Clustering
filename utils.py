86669387be9eb972bbebc56196b82d091075b57f
Ido-c <its.ido@gmail.com>
2021-02-24 15:19:05 +0200

    because Jacob asked

--- utils.py
+++ utils.py
@@ -25,12 +25,12 @@ def MGS(A, n):
 # QR iteration
 
 # The Eigengap Heuristic
-def Eigengap(values):
+def eigengap(values):
     sorted = np.sort(values)
     index = 0
     max = -1
     for i in range(len(sorted)//2):
-        temp = abs(sorted[i]-sorted[i + 1])
+        temp = abs(sorted[0, i]-sorted[0, i + 1])
         if temp > max:
             max = temp
             index = i
