fe0c1d7d5dfbf9d00494b0ac629a4ec41d964f88
jacob <jacobgidron@mail.tau.ac.il>
2021-02-24 15:19:12 +0200

    change K means-PP

--- utils.py
+++ utils.py
@@ -10,26 +10,23 @@ def MGS(A, n):
     for i in range(n):
         temp = np.linalg.norm(U[:, i])
         R[i, i] = temp
-        if temp ==0 :
-            print(" 0 divvvvvvvvvvv")
-        temp = 1/temp
-        col = U[:, i]*temp
+        temp = 1 / temp
+        col = U[:, i] * temp
         Q[:, i] = col
-        for j in range(i+1, n):
+        for j in range(i + 1, n):
             Rij = col @ U[:, j]
             R[i, j] = Rij
-            U[:, j] = U[:, j] - Rij*col
+            U[:, j] = U[:, j] - Rij * col
     return (Q, R)
 
-# QR iteration
 
 # The Eigengap Heuristic
 def Eigengap(values):
     sorted = np.sort(values)
     index = 0
     max = -1
-    for i in range(len(sorted)//2):
-        temp = abs(sorted[i]-sorted[i + 1])
+    for i in range(len(sorted) // 2):
+        temp = abs(sorted[i] - sorted[i + 1])
         if temp > max:
             max = temp
             index = i
@@ -43,21 +40,22 @@ def save_data(vectors, clusters, d):
     np.savetxt("data.txt", data, delimiter=", ", fmt=fmt)
 
 
-
-
+# QR iteration
 def QR_iteration_algorithm(A):
     n = A.shape[0]
     Q_bar = np.identity(n)
     for i in range(n):
-        Q, R = MGS(A,A.shape[0])
+        Q, R = MGS(A, A.shape[0])
         A = R @ Q
         new_Q_bar = Q_bar @ Q
         ep = (np.absolute(Q_bar) - np.absolute(new_Q_bar)).max()
         if ep < 0.0001:
-            return (A,Q_bar)
+            break
         Q_bar = new_Q_bar
-    return (A,Q_bar)
+        eigenvalues = np.array([A[i, i] for i in range(n)], dtype=np.float64)
+    return (eigenvalues, Q_bar)
 
-A = np.array([0, 12, 3, 4, 5, 6, 7, 8, 9],dtype=np.float64).reshape(3, 3)
+
+A = np.array([0, 12, 3, 4, 5, 6, 7, 8, 9], dtype=np.float64).reshape(3, 3)
 x = QR_iteration_algorithm(A)
 print(x)
