e31f0a1e98840bd18413040d7ae50513d4cd65ae
jacob <jacobgidron@mail.tau.ac.il>
2021-03-01 12:15:56 +0200

    fix overlapping text in main

--- main.py
+++ main.py
@@ -37,16 +37,12 @@ def main(n, k, random):
     # Print max capacity
     # Generate random data with indexed data points
     vectors, clusters = make_blobs(n_samples=n, n_features=d, centers=k)
-    print(len(clusters[clusters == 0]))
-    vectors,clusters = make_blobs(n_samples=n,n_features=d,centers=k)
-    x, k = spectral_clustering.spectral_clustering(vectors, n, d) #todo erase x, k
-    print("k is",k)
-    print("now ony kmeans") #todo
-    kmeans_pp.k_means_pp(vectors, k, d, n, 300)
+    print("the sizes of the real custers : ",[len(clusters[clusters == i]) for  i in range (k)])
     # Create 1st txt file
     utils.save_data(vectors, clusters, d)
 
     # Run Spectral Clustering and put clusters in 2nd file
+    print("k is",k)
     x, obs_k = spectral_clustering.spectral_clustering(vectors, n, d)  # todo erase x, k
     spectral_clusters = utils.create_cluster_vector(x, n, obs_k)
 
@@ -70,17 +66,12 @@ def main(n, k, random):
     utils.save_to_pdf(vectors, spectral_clusters, kmeans_clusters, d, k, n, obs_k, sjm, kjm)
 
 
-# Compute Jaccard measure for both algo's and put in pdf
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('n', type=int, help="# of vectors")
     parser.add_argument('k', type=int, help="# of clusters")
     parser.add_argument('--Random',  help="random k and n",default=True,action='store_false')
     args = parser.parse_args()
-    #random = (args.random == "True") # todo check for a better solution
-    # k = args.k
-    # n = args.n
 
     print("n is:  ", args.n)
     print("k is:  ", args.k)
