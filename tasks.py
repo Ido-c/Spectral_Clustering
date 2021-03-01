5db23afc83f64d36ccb5b2bc2c7616b92afce1a7
jacob <jacobgidron@mail.tau.ac.il>
2021-03-01 11:11:42 +0200

    fix bug in run

--- tasks.py
+++ tasks.py
@@ -12,7 +12,8 @@ def delete(c):
 
 
 @task(name='run', optional=['Random'], )
-def run(c, k=0, n=0, Random=False):
+def run(c, k, n, Random=False):
     print("building shared object files")
     c.run("python setup.py build_ext --inplace")  # todo
-    c.run("python main.py {n:} {k:} -- {random:}".format(n=n, k=k, random=str(Random)))  # todo
+    print("python main.py {n:} {k:} {random:}".format(n=n, k=k, random=("--Random" if Random else "")))  # todo
+    c.run("python main.py {n:} {k:} {random:}".format(n=n, k=k, random=("--Random" if Random else "")))  # todo
